654534e8a5e45f8331de510674cc87a85b79f5a9
wathmal96 <ranaweera1125vinura@gmail.com>
2024-03-05 17:27:44 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        var video = new Video();
+        video.setFileName("birthDay.mp4");
+        video.setTitle("jennifer's birthday");
+        video.setUser(new User("john@domain"));
+
+        var processor = new VideoProcessor();
+        processor.process(video);
+    }
+}
